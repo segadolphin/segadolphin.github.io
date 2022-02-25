1bc7ac9afb31b5e171871664948bcf3a4462e52a
Унжаков Сергей <34319331+segadolphin@users.noreply.github.com>
2022-02-26 01:31:44 +0300

    Delete Опросник directory

--- Опросник/js/control.js
+++
@@ -1,6 +0,0 @@
-O("select-experience").onchange = myChangeFunction
-
-function myChangeFunction() {
-    this.value == "yes" ? O("textarea-experience").hidden = false
-        : O("textarea-experience").hidden = true;
-}
